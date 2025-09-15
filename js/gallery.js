df7c6cac44cb5156a12308f61c3b991d941ba274
Andrew Gubsky <andrei.gubsky@gmail.com>
2025-09-15 14:23:26 +0300

    Initial commit

---
+++ js/gallery.js
@@ -0,0 +1,108 @@
+const images = [
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+        description: 'Hokkaido Flower',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+        description: 'Container Haulage Freight',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+        description: 'Aerial Beach View',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+        description: 'Flower Blooms',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+        description: 'Alpine Mountains',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+        description: 'Mountain Lake Sailing',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+        description: 'Alpine Spring Meadows',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+        description: 'Nature Landscape',
+    },
+    {
+        preview:
+            'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+        original:
+            'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+        description: 'Lighthouse Coast Sea',
+    },
+];
+
+function createMarkup() {
+
+    const li = document.createElement('li');
+    li.setAttribute('class', 'gallery-item');
+
+    const a = document.createElement('a');
+    a.setAttribute('class', 'gallery-link');
+    a.setAttribute('href', 'large-image.jpg');
+
+    const img = document.createElement('img');
+    img.setAttribute('class', 'gallery-image');
+    img.setAttribute('src', 'small-image.jpg');
+    img.setAttribute('alt', 'Image description');
+    img.dataset.source = 'large-image.jpg';
+
+    const ul = document.querySelector('ul.gallery');
+    ul.appendChild(li).appendChild(a).appendChild(img);
+
+
+
+
+    ul.addEventListener('click', (event) => {
+        //event.target.matches('.gallery-image')
+        console.log(event);
+    });
+}
+createMarkup()
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p style="display:block; color:white; text-align: center;">A custom modal.</p>
+        <a style="display:block; color:white; text-align: end;">Close</a>
+        <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg" width="800" height="600">
+        
+    </div>
+`, {
+    onShow: (instance) => {
+        instance.element().querySelector('a').onclick = instance.close;
+    }
+})
+
+instance.show()
